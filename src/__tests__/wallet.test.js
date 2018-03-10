a51f9873cc5efae30813bcb33d3c77e381045f39
Daniel Keen <keendaniel01@gmail.com>
2018-03-10 16:06:48 +0000

    user can input a number into .input-balance and update wallet component state to match the number

--- src/__tests__/wallet.test.js
+++ src/__tests__/wallet.test.js
@@ -13,4 +13,21 @@ describe('Wallet', () => {
   it('gets the balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Balance: 10');
   });
+
+  it('displays a form that the user can type into to update balance', () => {
+    expect(wallet.find('.input-balance').exists()).toBe(true);
+  });
+
+  describe('when a user types into .input-balance', () => {
+    const userBalance = 25;
+    beforeEach(() => {
+      wallet
+        .find('.input-balance')
+        .simulate('change', { target: { value: userBalance } });
+    });
+
+    it('updates the local `state` balance and converts to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+  });
 });
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.state = { balance: undefined };
+  }
+
+  updateBalance = event =>
+    this.setState({ balance: parseInt(event.target.value, 10) });
+
   render() {
     return (
       <div>
         <h3 className="balance">Balance: {this.props.balance}</h3>
+        <input className="input-balance" onChange={this.updateBalance} />
       </div>
     );
   }
