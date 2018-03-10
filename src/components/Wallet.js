3ae0299c3ea6557191707aaafabe3ca876b4db78
Daniel Keen <keendaniel01@gmail.com>
2018-03-10 11:58:39 +0000

    Wallet component is rendering

---
+++ src/components/Wallet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wallet extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Balance: </h3>
+      </div>
+    );
+  }
+}
+
+export default Wallet;
