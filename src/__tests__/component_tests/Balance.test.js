b81ba53038efae2c585a6f57f7d7f54dbb8940c4
Daniel Keen <keendaniel01@gmail.com>
2018-04-02 16:47:44 +0100

    test: Balance component renders user balance :white_check_mark:

--- src/__tests__/component_tests/Balance.test.js
+++ src/__tests__/component_tests/Balance.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme';
 import Balance from '../../components/Balance';
 
 describe('Balance', () => {
-  let balance = shallow(<Balance />);
+  let props = { userBalance: 10 };
+  let balance = shallow(<Balance {...props} />);
 
   it('renders correctly', () => {
     expect(balance).toMatchSnapshot();
+  });
+
+  it('renders the user balance', () => {
+    expect(balance.text()).toEqual('Balance: 10');
   });
 });
