3f8db907a8d044b8c1c896f34b336444e73f0ad3
Daniel Keen <keendaniel01@gmail.com>
2018-04-02 14:50:44 +0100

    feat: Wrapper renders a Wallet and Coin Component

--- src/containers/Wrapper.js
+++ src/containers/Wrapper.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Wallet from '../components/Wallet';
+import Coins from '../components/Coins';
 
 class Wrapper extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Wallet />
+        <Coins />
       </div>
     );
   }
