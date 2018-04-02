b27f295f6b9270aa2c1b8cadb863bc915e3a8315
Daniel Keen <keendaniel01@gmail.com>
2018-04-02 17:29:19 +0100

    test: ChangeBalance calls changeBalance prop when a user types into input :white_check_mark:

--- src/__tests__/component_tests/ChangeBalance.test.js
+++ src/__tests__/component_tests/ChangeBalance.test.js
@@ -3,9 +3,18 @@ import { shallow } from 'enzyme';
 import ChangeBalance from '../../components/ChangeBalance';
 
 describe('ChangeBalance', () => {
-  const changeBalance = shallow(<ChangeBalance />);
+  const mockHandleUpdateBalance = jest.fn();
+  const props = { changeBalance: mockHandleUpdateBalance };
+  const changeBalance = shallow(<ChangeBalance {...props} />);
 
   it('renders correctly', () => {
     expect(changeBalance).toMatchSnapshot();
+  });
+
+  describe('when a user inputs something into .input-balance', () => {
+    it('calls handleUpdateBalance()', () => {
+      changeBalance.find('.input-balance').simulate('change');
+      expect(mockHandleUpdateBalance).toHaveBeenCalled();
+    });
   });
 });
