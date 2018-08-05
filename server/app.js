1a855c3b9730d058103dc3dde329de52daf6298e
Daniel Keen <keendaniel01@gmail.com>
2018-08-05 16:12:53 +0100

    Setup mongoDB instance and connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const keys = require('./keys.js')
 
 const app = express();
+
+mongoose.connect(`mongodb://${keys.DB_USER}:${keys.DB_PASSWORD}@ds113482.mlab.com:13482/crypto-tracker-dev`)
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
